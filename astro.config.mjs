c56e657bf212ee3709e35f3aa61030b818c80ea6
ilies Tabyaoui <tbyilies@gmail.com>
2025-04-22 21:18:55 +0200

    Ajout de nouvelles fonctionnalités

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site:"https://github.com/iliestby.github.io",
+  base :"PortfolioIlies",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
